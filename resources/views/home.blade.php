96f35b83fce8dacbd8c1e7076655a15cac4d4a6e
unknown <naraedza@gmail.com>
2017-09-21 02:39:34 +0700

    update ui

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,21 +3,124 @@
 @section('content')
 <div class="container">
     <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Dashboard</div>
-
-                <div class="panel-body">
-                    @if (session('status'))
-                        <div class="alert alert-success">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
-                </div>
+    <div class="col-md-10 col-md-offset-1">
+    <!-- Widget: user widget style 1 -->
+    <div class="box box-widget widget-user">
+      <!-- Add the bg color to the header using any of the bg-* classes -->
+      <div class="widget-user-header bg-black" style="background: url('/bower_components/AdminLTE/dist/img/photo1.png') center center;">
+        <h3 class="widget-user-username">{{ ucfirst(Auth::user()->name) }}</h3>
+        <h5 class="widget-user-desc">Web Designer</h5>
+      </div>
+      <div class="widget-user-image">
+        <img class="img-circle" src="/bower_components/AdminLTE/dist/img/user3-128x128.jpg" alt="User Avatar">
+      </div>
+      <div class="box-footer">
+        <div class="row">
+          <div class="col-sm-4 border-right">
+            <div class="description-block">
+              <h5 class="description-header">1</h5>
+              <span class="description-text">POST</span>
             </div>
+            <!-- /.description-block -->
+          </div>
+          <!-- /.col -->
+          <div class="col-sm-4 border-right">
+            <div class="description-block">
+              <h5 class="description-header">13,000</h5>
+              <span class="description-text">FOLLOWERS</span>
+            </div>
+            <!-- /.description-block -->
+          </div>
+          <!-- /.col -->
+          <div class="col-sm-4">
+            <div class="description-block">
+              <h5 class="description-header">35</h5>
+              <span class="description-text">PRODUCTS</span>
+            </div>
+            <!-- /.description-block -->
+          </div>
+          <!-- /.col -->
+    </div>
+    </div>
+</div>
+<!--modals-->
+<div class="row margin-1per">
+    <div class="clearfix">
+        <div class="col-md-12">
+            <div class="pull-right">
+            <button type="button" class="btn btn-danger" data-toggle="modal" data-target="#modal-danger">
+                        Want to Sell
+                </button>
+            </div>
+        
+        <div class="pull-left">
+            <button type="button" class="btn btn-success" data-toggle="modal" data-target="#modal-success">
+                    Want to Buy
+            </button>
+        </div>
         </div>
     </div>
 </div>
+
+<!-- showmodals-red -->
+<div class="modal modal-danger fade" id="modal-danger">
+          <div class="modal-dialog">
+            <div class="modal-content">
+              <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                  <span aria-hidden="true">&times;</span></button>
+                <h4 class="modal-title">Danger Modal</h4>
+              </div>
+              <div class="modal-body">
+                <p>One fine body&hellip;</p>
+              </div>
+              <div class="modal-footer">
+                <button type="button" class="btn btn-outline pull-left" data-dismiss="modal">Close</button>
+                <button type="button" class="btn btn-outline">Save changes</button>
+              </div>
+            </div>
+          </div>
+        </div>
+<!-- showmodal-red -->
+<!-- showmodal-green -->
+<div class="modal modal-success fade" id="modal-success">
+          <div class="modal-dialog">
+            <div class="modal-content">
+              <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                  <span aria-hidden="true">&times;</span></button>
+                <h4 class="modal-title">Success Modal</h4>
+              </div>
+              <div class="modal-body">
+                <p>One fine body&hellip;</p>
+              </div>
+              <div class="modal-footer">
+                <button type="button" class="btn btn-outline pull-left" data-dismiss="modal">Close</button>
+                <button type="button" class="btn btn-outline">Save changes</button>
+              </div>
+            </div>
+            <!-- /.modal-content -->
+          </div>
+          <!-- /.modal-dialog -->
+        </div>
+        <!-- showmodal-green -->
+<!-- /.modal -->
+
+<!--box-->
+<div class="row margin-1per">
+    <div class="col-md-4">
+        <div class="box">
+        </div>>
+    </div>
+    <div class="col-md-4">
+        <div class="box">
+        </div>>
+    </div>
+    <div class="col-md-4">
+        <div class="box">
+        </div>>
+    </div>
+</div>
+<!--box-->
+
 @endsection
