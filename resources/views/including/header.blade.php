57be70502d57bc0ebd1866d15f94fd302ac28177
zack13110 <nutthakornzack1@gmail.com>
2017-09-21 14:24:34 +0700

    update merge

--- resources/views/including/header.blade.php
+++ resources/views/including/header.blade.php
@@ -2,7 +2,15 @@
 <head>
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Laravel') }}</title>
+
+    <!-- Styles -->
+  <link href="{{ asset('css/app.css') }}" rel="stylesheet">
   <title> Recypid</title>
   <!-- Tell the browser to be responsive to screen width -->
   <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,19 +1,21 @@
 <!DOCTYPE html>
 <html lang="{{ app()->getLocale() }}">
-<head>
+<!--head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <!-- CSRF Token -->
+-->
+    <!-- CSRF Token 
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <title>{{ config('app.name', 'Laravel') }}</title>
-
-    <!-- Styles -->
+    -->
+    <!-- Styles
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
-<body>
+ -->
+ @include('including/header')
+<body class="first-bg">
     <div id="app">
         <nav class="navbar navbar-default navbar-static-top">
             <div class="container">
