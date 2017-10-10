25ae9195e223eec0749b2329c08c19621911c674
zack13110 <nutthakornzack1@gmail.com>
2017-10-11 01:49:54 +0700

    add calculate

--- app/Http/Controllers/sellController.php
+++ app/Http/Controllers/sellController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Request;
 use Auth;
 use DB;
+use App\sell;
 
 class sellController extends Controller
 {
@@ -31,14 +32,25 @@ class sellController extends Controller
             $gender_x = $key->gender;
             $time_x = $key->time;
 
-            $data_buyer_matching = DB::table('buys')->where( ['type'=> $type])->get();
+            $data_buyer_matching = DB::table('buys')->where( ['type'=> $type_x])->get();
             foreach ($data_buyer_matching as $key_){
                 $type_y = $key_->type;
                 $sub_type_y = $key_->sub_type;
                 $gender_y = $key_->gender;
                 $time_y = $key_->time;
-
+                echo '<pre>';
+                print_r($data_buyer_matching);
                 /*calculate */
+                $cal_xdoty = ($sub_type_x*$sub_type_y)+($gender_x*$gender_y)+($time_x*$time_y);
+                $cal_xy = sqrt(pow($sub_type_x,2)+pow($gender_x,2)+pow($time_x,2)) * sqrt(pow($sub_type_y,2)+pow($gender_y,2)+pow($time_y,2));
+                $cal = $cal_xdoty / $cal_xy;
+                
+                echo($cal_xdoty); 
+                echo '<br>';
+                echo($cal_xy);
+                echo '<br>';
+                echo($cal);
+
             }
         }
         exit();
